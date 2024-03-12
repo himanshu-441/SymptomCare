cb4d34b0968dec17d1c63f8ed1060c96828fa975
Himanshu <hs127010@gmail.com>
2024-03-12 13:20:23 +0530

    added remote

--- disease_prediction/settings.py
+++ disease_prediction/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'predico',
         'USER': 'postgres',
-        'PASSWORD': '1234',
+        'PASSWORD': 'Aman@123',
         'HOST': 'localhost'
     }
 }
